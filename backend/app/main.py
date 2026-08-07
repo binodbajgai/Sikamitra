609c94d9450f08acc2edcc0e1c1b16a4d049f746
Binod Bajgai <bajgaibenod@gmail.com>
2026-08-07 23:34:02 +0545

    feat: initialize FastAPI backend foundation

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.api.router import router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
+
+app.include_router(router)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to Sikamitra API"
+    }
